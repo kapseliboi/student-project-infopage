3774c81ab54694d460aa15c7b145025d0bfd30aa
Akseli Käppi <akseli.kappi@gmail.com>
2019-09-25 23:11:53 +0300

    Added initial routing and some test text

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import 'typeface-roboto';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { Route } from 'react-router-dom';
+import Frontpage from './components/Frontpage';
 
 import Navbar from './components/Navbar';
+import { Toolbar } from '@material-ui/core';
 
 const App: React.FC = () => {
+  const arr = [];
+  for (let i = 0; i < 50; i++) {
+    arr.push('Super!');
+  }
   return (
     <React.Fragment>
       <CssBaseline />
-      <Navbar />
-      <div className="App">
-      </div>
+      <Route path='*' component={Navbar} />
+      <Toolbar />
+      <Route path='/' component={Frontpage} />
+      {arr.map( (test) => <p>{test}</p> )}
     </React.Fragment>
   );
 }
