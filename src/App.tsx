bb62f0bd8576b301a416e4ead34cf9a968bf08b2
Akseli Käppi <akseli.kappi@gmail.com>
2019-09-27 00:38:16 +0300

    Removed test code

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,12 @@ import Frontpage from './components/Frontpage';
 import Navbar from './components/Navbar';
 import { Toolbar } from '@material-ui/core';
 
-const App: React.FC = () => {
-  const arr = [];
-  for (let i = 0; i < 50; i++) {
-    arr.push('Super!');
-  }
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Route path='*' component={Navbar} />
-      <Toolbar />
-      <Route path='/' component={Frontpage} />
-      {arr.map( (test) => <p>{test}</p> )}
-    </React.Fragment>
-  );
-}
+const App: React.FC = () =>
+  <React.Fragment>
+    <CssBaseline />
+    <Route path='*' component={Navbar} />
+    <Toolbar />
+    <Route path='/' component={Frontpage} />
+  </React.Fragment>
 
 export default App;
