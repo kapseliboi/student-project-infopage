8250d5b985dab525317360bcba4b84612797da37
Akseli Käppi <akseli.kappi@gmail.com>
2019-09-26 17:19:01 +0300

    Add background image

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -2,14 +2,34 @@ import * as React from 'react';
 import { Container, Typography, makeStyles, Theme, createStyles } from '@material-ui/core';
 
 import * as texts from '../assets/pageTexts.json';
+const heroBackground = require('../assets/kansiareena.jpg');
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
     hero: {
-        marginTop: theme.spacing(20),
+        paddingTop: theme.spacing(20),
+        paddingBottom: theme.spacing(20),
+        textShadow: '2px 2px #000000',
+        color: '#ffffff',
+        background: 'rgba(41, 44, 61, 0.2)'
+    },
+    heroImage: {
+        background: `url(${heroBackground}) center fixed`,
+        position: 'absolute',
+        width: '100%',
+        height: '1000px', // 100% depends on the screen size, causes issues
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: '0% 70%',
+        zIndex: -1,
     },
     heroText: {
         marginBottom: theme.spacing(5),
-        marginTop: theme.spacing(5),
+        fontWeight: 400,
+    },
+    heroContainer: {
+        background: 'rgba(41, 44, 61, 0.5)',
+        paddingTop: theme.spacing(10),
+        paddingBottom: theme.spacing(5),
     },
     introduction: {
         background: '#4e008e',
@@ -18,27 +38,41 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
         paddingBottom: theme.spacing(10),
         borderTop: '1px solid black',
         borderBottom: '1px solid black',
-    }
+    },
+    why: {
+        background: '#ffffff',
+        paddingTop: theme.spacing(10),
+        paddingBottom: theme.spacing(10),
+    },
 }));
 
 const Frontpage: React.FC = () => {
     const classes = useStyles();
     return (
         <>
-            <section>
-                <Container className={classes.hero}>
+            <div className={classes.heroImage} />
+            <section className={classes.hero}>
+                <div className={classes.heroContainer}>
                     <Typography variant='h2' align='center'>
                         {texts.frontPage.heroHeader}
                     </Typography>
                     <Typography variant='h5' align='center' className={classes.heroText}>
                         {texts.frontPage.heroText}
                     </Typography>
-                </Container>
+                </div>
             </section>
             <section className={classes.introduction}>
                 <Container>
-                    <Typography variant='h6' align='center'>
+                    <Typography variant='h5' align='center'>
                         {texts.frontPage.introduction}
+                    </Typography>
+
+                </Container>
+            </section>
+            <section className={classes.why}>
+                <Container>
+                    <Typography variant='h4' align='center'>
+                        {texts.frontPage.why}
                     </Typography>
                 </Container>
             </section>
